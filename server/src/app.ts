ef041851f2b813e16146b42771ee4620226e228d
IM-Suryakant-Kumar <ksuryakant43@gmail.com>
2026-01-23 21:09:11 +0530

    Initial Commit

---
+++ server/src/app.ts
@@ -0,0 +1,25 @@
+import "dotenv/config";
+import express from "express";
+import helmet from "helmet"
+import cors from "cors"
+import morgan from "morgan"
+
+const app = express();
+const PORT = process.env.PORT;
+const CLIENT_URL = process.env.CLIENT_URL;
+
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+app.use(helmet())
+app.use(cors({origin: CLIENT_URL, credentials: true}))
+app.use(morgan("tiny"))
+
+app.listen(PORT, async () => {
+  try {
+    console.log("App is running on http://localhost:4000")
+  } catch (error) {
+    console.log(error);
+  }
+})
+
+export default app;
